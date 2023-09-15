f5dadad1b786534606b9020527f93134e197eca4
Stanley643 <nmurigi@kabarak.ac.ke>
2023-09-15 22:06:12 +0300

    dynamic memory allocations

---
+++ dynamic_memory.c
@@ -0,0 +1,14 @@
+/**
+ * The process of allocating memory at the time of execution is called dynamic memory allocation.
+ *
+ * Heap is the segment of memory where dynamic memory allocation takes place.
+ *
+ * unlike stack where memory is allocated or deallocated ina defined order, heap 
+ * is an ares of memory where memory is allocated or deallocated without any order or randomly.
+ *
+ * Built in functions for dynamic memory allocation include:
+ * malloc()
+ * calloc()
+ * realloc()
+ * free()
+ */
