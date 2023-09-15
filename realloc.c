96c92743621e6b95e3858bcefbc19d4f33bd1b1e
Stanley643 <nmurigi@kabarak.ac.ke>
2023-09-15 22:57:08 +0300

    realloc

--- realloc.c
+++ realloc.c
@@ -14,4 +14,40 @@
  * 		ptr = (int *)realloc(ptr, 2*sizeof(int));
  *
  */
+#include<stdio.h>
+#include<stdlib.h>
 
+int main()
+{
+	int i;
+	int *ptr = (int *)malloc(2*sizeof(int));
+
+	if (ptr == NULL)
+	{
+		printf("Memory not available!");
+		exit(1);
+	}
+
+	printf("Enter the two numbers: \n");
+	for (i = 0; i < 2; i++)
+		scanf("%d", ptr + i);
+
+	//memory allocation for 2 more integers
+	
+	ptr = (int *)realloc(ptr, 4*sizeof(int));
+	if (ptr == NULL)
+	{
+		printf("Memory not available");
+		exit(1);
+	}
+
+	printf("Enter 2 more integers: \n");
+	for (i = 2; i < 4; i++)
+		scanf("%d", ptr +i);
+
+
+	//printing the values
+	for(i = 0; i<4; i++)
+		printf("%d ", *(ptr + i));
+	return 0;
+}
