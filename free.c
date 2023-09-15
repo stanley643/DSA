ebea6c85813efe2c9bb067b61ff7ba8f632b85a7
Stanley643 <nmurigi@kabarak.ac.ke>
2023-09-15 23:09:09 +0300

    free

---
+++ free.c
@@ -0,0 +1,32 @@
+/**
+ * free() function is used to release the dynamically allocated memory in heap.
+ * SYNTAX:
+ * 	void free(ptr);
+ *
+ */
+#include<stdio.h>
+#include<stdlib.h>
+
+int *input()
+{
+	int *ptr, i;
+	ptr = (int *)malloc(5*sizeof(int));
+	printf("Enter 5 numbers: ");
+	for(i = 0; i < 5; i++)
+		scanf("%d", ptr + i);
+
+	return ptr;
+}
+
+int main()
+{
+	int i, sum = 0;
+	int *ptr = input();
+	for(i = 0; i < 5; i++)
+		sum += *(ptr + i);
+
+	printf("Sum is: %d", sum);
+        free(ptr); //releasing the memory at the end
+  	ptr = NULL;
+	return 0;
+}
