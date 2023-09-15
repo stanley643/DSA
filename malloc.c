f154f04e3e527bd65a05bea6477772f4d85d0251
Stanley643 <nmurigi@kabarak.ac.ke>
2023-09-15 22:11:34 +0300

    malloc

---
+++ malloc.c
@@ -0,0 +1,7 @@
+/*
+ * malloc is a built in function declared in the header file <stdlib.h>
+ *
+ * SYNTAX:  (void* )malloc(size_t size)
+ * size_t is defined is <stdlib.h> as unsigned int.
+ *
+ */
