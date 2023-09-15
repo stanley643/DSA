e3473ba85d41ccf03ed6ab1acc398eff5f2bf042
Stanley643 <nmurigi@kabarak.ac.ke>
2023-09-15 21:58:11 +0300

    static memory

---
+++ static_memory.c
@@ -0,0 +1,12 @@
+/**
+ * memory allocated during compile time is called static memory.
+ * the memory allocated is fixed and cannot be increased or decreased during run time.
+ *
+ * eg.
+ */
+#include<stdio.h>
+
+int main()
+{
+	int arr[5] = {1, 2, 3, 4, 5};
+}
