224b2f6e6d27cb68c3831ace69cc66297f27a2bd
Stanley643 <nmurigi@kabarak.ac.ke>
2023-09-17 21:24:16 +0300

    _putchar

--- Structures_fns/_putchar.c
+++ Structures_fns/_putchar.c
@@ -1,6 +1,31 @@
 #include "main.h"
 
+/**
+ * _putchar - writes the character c to stdout
+ * @c: Ther character to print
+ * Return: On success 1 and on error -1
+ */
 int _putchar( char c)
 {
 	return (write(1, &c, 1));
+}
+
+/**
+ * print_string - function that calls the _putchar
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ * @string: characters passed
+ * Return: 1 on success and -1 on error
+ */
+
+void print_string(char *string)
+{
+	int i = 0;
+
+	while (string[i] != '\0')
+	{
+		_putchar(string[i]);
+		i++;
+	}
+
 }
