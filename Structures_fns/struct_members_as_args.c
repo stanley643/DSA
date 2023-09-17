9129715d9378c7a6c202ba246a0e3abb4e061471
Stanley643 <nmurigi@kabarak.ac.ke>
2023-09-17 21:13:05 +0300

    structs as args

---
+++ Structures_fns/struct_members_as_args.c
@@ -0,0 +1,24 @@
+/**
+ * just like variables, we can pass structure members as arguments to a function
+ *
+ */
+#include "main.h"
+
+struct student
+{
+	char name[50];
+	int age;
+	int roll_no;
+	float marks;
+};
+
+void print(char name[], int age, int roll, float marks)
+{
+	printf("%s %d %d %.2f", name, age, roll, marks);
+}
+
+int main()
+{
+	struct student s1 = {"Stano", 20, 50, 86.5};
+	print(s1.name, s1.age, s1.roll_no, s1.marks);
+}
