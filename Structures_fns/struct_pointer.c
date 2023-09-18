3f8ca54f25dc9fa4613981be9fc23a1f6ef9f65f
Stanley643 <nmurigi@kabarak.ac.ke>
2023-09-18 22:36:05 +0300

    pointer

--- Structures_fns/struct_pointer.c
+++ Structures_fns/struct_pointer.c
@@ -1,9 +1,7 @@
 #include "main.h"
 /**
  * instead of passing structure members individually,
- * it is a good practice to pass a structure variable as an argument.
- *
- * unlike arrays, name of the structure variables are not pointers
+ * it is a good practice to pass a structure  as a pointer if the struct is tooo big.
  *
  */
 
