0e8b5c2b21fa18f03d92ea87dbf1bea76900409c
Stanley643 <nmurigi@kabarak.ac.ke>
2023-09-18 22:33:40 +0300

    pointer

--- Structures_fns/struct_pointer.c
+++ Structures_fns/struct_pointer.c
@@ -13,16 +13,16 @@ struct point
 	int y;
 };
 
-void print(struct point p)
+void print(struct point *ptr)
 {
-	printf("%d %d\n", p.x, p.y);
+	printf("%d %d\n", ptr->x, ptr->y);
 }
 
 int main()
 {
 	struct point p1 = {23, 45};
 	struct point p2 = {56, 90};
-	print(p1);
-	print(p2);
+	print(&p1);
+	print(&p2);
 	return 0;
 }
