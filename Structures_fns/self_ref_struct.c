cc75745f21e2e1d7885e8116e0640692ec5f5af3
Stanley643 <nmurigi@kabarak.ac.ke>
2023-09-23 21:35:43 +0300

    r

--- Structures_fns/self_ref_struct.c
+++ Structures_fns/self_ref_struct.c
@@ -19,7 +19,13 @@ struct code
 int main()
 {
 	struct code var1;
+	struct code var2;
 
 	var1.i = 65;
 	var1.c = 'A';
 	var1.ptr = NULL;
+
+	var2.i = 66;
+        var2.c = 'B';
+        var2.ptr = NULL;
+
