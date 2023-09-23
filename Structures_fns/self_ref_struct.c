49cb789f6561fb50821a81ad62915fc586772edf
Stanley643 <nmurigi@kabarak.ac.ke>
2023-09-23 21:25:35 +0300

    r

---
+++ Structures_fns/self_ref_struct.c
@@ -0,0 +1,8 @@
+/**
+ * self referential structures are those structures in which one or more pointers point to the structure of the same type.
+ * syntax:
+ * 	struct self {
+ * 		int p;
+ * 		struct self *ptr;
+ * 	};
+ */
