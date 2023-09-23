eae08b8c3c7d6d564f19e9544dfbca839cdcf28a
Stanley643 <nmurigi@kabarak.ac.ke>
2023-09-23 22:36:08 +0300

    linked list

--- linked_lists/s_linked-list.c
+++ linked_lists/s_linked-list.c
@@ -5,5 +5,11 @@ int main()
 	struct node *head = malloc(sizeof(struct node));
 	head->data = 45;
 	head->link = NULL;
+
+	struct node *second = malloc(sizeof(struct node));
+	second->data = 98;
+	second->link = NULL;
+	head->link = second;
+
 	return 0;
 }
