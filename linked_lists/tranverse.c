ad703c254dd685c363c4b18f4daf57f0bc36f42a
Stanley643 <nmurigi@kabarak.ac.ke>
2023-09-24 20:13:01 +0300

    l

---
+++ linked_lists/tranverse.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+int main()
+{
+	struct node *head = malloc(sizeof(struct node));
+	head->data = 45;
+	head->link = NULL;
+
+	struct node *current = malloc(sizeof(struct node));
+	current->data = 98;
+	current->link = NULL;
+	head->link = current;
+	
+	current = malloc(sizeof(struct node));
+	current->data = 3;
+	current->link = NULL;
+
+	head->link->link = current;
+	return 0;
+}
